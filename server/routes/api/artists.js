5b221931fd0a57c5e5ed1d908470c2f5d9f63572
Biggs Thorarensen <biggs@devillion.com>
2020-02-23 16:22:30 +0000

    artists endpoint setup (get, post and delete)

--- server/routes/api/artists.js
+++ server/routes/api/artists.js
@@ -1,19 +1,40 @@
 const express = require('express')
 const mongodb = require('mongodb')
+const slugify = require('slugify')
 
 const router = express.Router()
 
 // Get Artists
-router.get('/', (req, res) => {
-    res.send('hello')
+router.get('/', async (req, res) => {
+    const artists = await loadArtistsCollection()
+    res.send(await artists.find({}).toArray())
 })
 
 // Add Artist
+router.post('/', async (req, res) => {
+    const artists = await loadArtistsCollection()
+    await artists.insertOne({
+        name: req.body.name,
+        slug: slugify(req.body.name),
+        createdAt: new Date()
+    })
+    res.status(201).send()
+})
 
 
 // Delete Artist
+router.delete('/:id', async (req, res) => {
+    const artists = await loadArtistsCollection()
+    await artists.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send()
+})
 
+async function loadArtistsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb://biggs_admin:nkqeoDHG10a6ccOg@audiofoodiedb-shard-00-00-vpqgw.mongodb.net:27017,audiofoodiedb-shard-00-01-vpqgw.mongodb.net:27017,audiofoodiedb-shard-00-02-vpqgw.mongodb.net:27017/devilliondb?retryWrites=true&ssl=true&authSource=admin&replicaSet=audiofoodiedb-shard-0', {useNewUrlParser: true
+    })
 
+    return client.db('devilliondb').collection('artists')
+}
 
 
 module.exports = router
