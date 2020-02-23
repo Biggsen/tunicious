3272c032342012de6a87cd97631ba18d92e74549
Biggs Thorarensen <biggs@devillion.com>
2020-02-23 15:42:32 +0000

    basic express setup, artists api prep

---
+++ server/routes/api/artists.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+// Get Artists
+router.get('/', (req, res) => {
+    res.send('hello')
+})
+
+// Add Artist
+
+
+// Delete Artist
+
+
+
+
+module.exports = router
