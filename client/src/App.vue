d414ff4469ee6466123de4abfb3b4c3aa003439a
Biggs Thorarensen <biggs@devillion.com>
2020-02-24 16:49:41 +0000

    vue router added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <Navbar />
-    <ArtistComponent />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-  import Navbar from './components/Navbar'
-
-  import ArtistComponent from './components/ArtistComponent.vue'
-
-  export default {
-    name: 'App',
-    components: {
-      Navbar,
-      ArtistComponent
-    }
-  }
-</script>
 
 <style>
 #app {
@@ -26,6 +15,18 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ArtistComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ArtistComponent from '@/components/ArtistComponent.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ArtistComponent
+  }
+}
+</script>
