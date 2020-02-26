368ffec8e874afd8eee1468d8c69dfc3d9b9c7d3
Biggs Thorarensen <biggs@devillion.com>
2020-02-26 09:34:35 +0000

    Navbar with login state

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav" class="p-6">
-      <router-link to="/" class="mr-4">Home</router-link>
-      <router-link to="/about" class="mr-4">About</router-link>
-      <router-link to="/login">Login</router-link>
-    </div>
+    <Navbar loggedIn="isUserLoggedIn" />
     <div class="p-6">
       <router-view/>
     </div>
   </div>
 </template>
+
+<script>
+  import Navbar from '@/components/Navbar'
+  export default {
+    data() {
+      return {
+        isUserLoggedIn: localStorage.getItem('user-token') ? true : false
+      }
+    },
+    components: {
+      Navbar
+    }
+  }
+</script>
 
 <style>
 #app {
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,15 +1,25 @@
 <template>
-    <h2>Navbar</h2>
+    <div class="flex">
+        <router-link to="/" class="mr-4">Home</router-link>
+        <router-link to="/about" class="mr-4">About</router-link>
+        <router-link to="/login" v-if="!loggedIn">Login</router-link>
+        <p v-if="loggedIn">Logged in</p>
+        <button v-on:click="logout()" v-if="loggedIn">Logout</button>
+    </div>
 </template>
 
 <script>
 export default {
 
   name: 'Navbar',
-
+  props: ['loggedIn'],
   data () {
-    return {
-
+    return {}
+  },
+  methods: {
+    logout: function() {
+        localStorage.removeItem('user-token')
+        this.$router.push('/login')
     }
   }
 }
