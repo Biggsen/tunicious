ac6ad83a7405481656514a766922ebb9d9ee0cc3
Biggs Thorarensen <biggs@devillion.com>
2020-03-06 07:42:29 +0000

    auth header setup by default if token found.

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+const token = localStorage.getItem('user-token')
+if (token) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   router,
