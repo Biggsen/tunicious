bf00cdf10c45606b16c115df26dbed8715bd3d22
Biggs <biggsdev@pm.me>
2024-01-14 22:26:06 +0000

    tailwind enabled on content, color and font family updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'mindero': '#FFFD98'
+    },
+    fontFamily: {
+      chivo: ['Chivo', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
