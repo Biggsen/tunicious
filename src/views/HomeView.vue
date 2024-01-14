c88415fd68ad9da8f69beda08611a36fe871af3e
Biggs <biggsdev@pm.me>
2024-01-14 22:29:15 +0000

    homeview updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+
 </script>
 
 <template>
 	<main>
-		<h1>Home</h1>
+		<h1 class="text-[36px] font-chivo font-bold">Homepage</h1>
+    <router-link to="/playlist">Playlists</router-link>
 	</main>
 </template>
