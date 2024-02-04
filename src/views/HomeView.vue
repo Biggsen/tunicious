6782da103bc966518a83fcaed3f12b145e6b0cdc
Biggs <biggsdev@pm.me>
2024-02-04 12:30:23 +0000

    generic token ref. logs for monitoring. loading state added as well.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,25 @@ import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
 import { getToken } from '../utils/api';
 
-const tokenFromStorage = localStorage.getItem('token');
-
-if (!tokenFromStorage) {
+const loading = ref(false)
+const token = ref(localStorage.getItem('token'))
+if (!token.value) {
+  console.log('token not been set');
+  loading.value = true;
   getToken().then(response => {
     localStorage.setItem('token', response.access_token)
+    console.log('token is now set');
+    token.value = response.access_token
+    loading.value = false;
   })
+} else {
+  console.log('token was already set');
 }
 </script>
 
 <template>
-	<main>
+  <p v-if="loading">Loading...</p>
+	<main v-else>
 		<h1 class="text-[36px] font-chivo font-bold">Homepage</h1>
 	</main>
 </template>
