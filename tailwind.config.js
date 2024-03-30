567f76e2a5f91447db2d02adea8504691306a542
Biggs <biggsdev@pm.me>
2024-03-30 16:01:15 +0000

    extend colors instead of replace

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      'mindero': '#FFFD98',
-      'delft-blue': '#23395B'
-    },
     fontFamily: {
-      chivo: ['Chivo', 'sans-serif'],
+      chivo: ["Chivo", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        mindero: "#FFFD98",
+        "delft-blue": "#23395B",
+      },
+    },
   },
   plugins: [],
-}
-
+};
