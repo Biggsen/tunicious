723b0fd71a13444f9febd33964bd2f94114564f5
Biggs <biggsdev@pm.me>
2024-04-02 19:58:18 +0100

    cleanup

--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -139,68 +139,6 @@ function onlyUnique(value, index, array) {
 const albumsCuriousIds = ref();
 const album = ref();
 const albumsCuriousData = ref({});
-
-//getTrackInfo(token.value).then(profile => { console.log(profile) })
-// getPlaylist(token.value).then(profile => { console.log(profile) })
-// getPlaylistItems(token.value).then(profile => {
-//   //console.log(profile)
-//   const albumIdArray = profile.items.map((elem) => {
-//     return elem.track.album.id
-//   })
-//   //console.log(albumIdArray);
-//   albumsCuriousIds.value = albumIdArray.filter(onlyUnique)
-//   console.log(albumsCuriousIds.value);
-//   const albumsCurios = [];
-//   albumsCuriousIds.value.forEach(album => {
-//     console.log(album);
-//     albumsCurios.push(
-//       getAlbum(token.value,album).then(data => {
-//         return data;
-//       })
-//     )
-//   })
-//   Promise.all(albumsCurios).then(result => {
-//     console.log(typeof result);
-//     albumsCuriousData.value = result
-//     console.log(albumsCuriousData.value);
-//   });
-
-//   //console.log(albumsCuriousData.value);
-// })
-// getAlbum(token.value,"1x55Z0fYARLdeJVjG2UESs").then(data => {
-//   album.value = data;
-//   //console.log(album.href);
-// })
-
-// function promiseAllProps(arrayOfObjects) {
-//     let datum = [];
-//     let promises = [];
-
-//     arrayOfObjects.forEach(function(obj, index) {
-//         Object.keys(obj).forEach(function(prop) {
-//             let val = obj[prop];
-//             // if it smells like a promise, lets track it
-//             if (val && val.then) {
-//                 promises.push(val);
-//                 // and keep track of where it came from
-//                 datum.push({obj: obj, prop: prop});
-//             }
-//         });
-//     });
-
-//     return Promise.all(promises).then(function(results) {
-//         // now put all the results back in original arrayOfObjects in place of the promises
-//         // so now instead of promises, the actaul values are there
-//         results.forEach(function(val, index) {
-//             // get the info for this index
-//             let info = datum[index];
-//             // use that info to know which object and which property this value belongs to
-//             info.obj[info.prop] = val;
-//         });
-//         // make resolved value be our original (now modified) array of objects
-//         return arrayOfObjects;
-//     });
-// }
 </script>
 
 <template>
@@ -225,14 +163,5 @@ const albumsCuriousData = ref({});
         <PlaylistItem :playlist="playlistKnownWonderful" />
       </ul>
     </div>
-    <!-- <ul class="flex flex-wrap gap-4">
-            <li v-for="album in albumsCuriousData" class="bg-mindero border-2 border-delft-blue rounded-xl p-2 pb-0">
-                <img :src="album.images[1].url" alt="" class="rounded-lg ">
-                <div class="p-3">
-                    <p class="font-chivo font-bold text-lg">{{ album.artists[0].name }}</p>
-                    <p class="font-chivo">{{ album.name }}</p>
-                </div>
-            </li>
-        </ul> -->
   </main>
 </template>
